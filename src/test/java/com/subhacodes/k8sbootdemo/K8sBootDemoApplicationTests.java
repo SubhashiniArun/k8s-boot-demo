bbdca537b4168c53a7613fca7719242e3dde2fca
SubhashiniArun <subhashini258@gmail.com>
2023-03-17 16:22:26 -0400

    Docker-k8s

---
+++ src/test/java/com/subhacodes/k8sbootdemo/K8sBootDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.subhacodes.k8sbootdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class K8sBootDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
